9f9cbf6c8be2c3b2448a8346be9c911fb4dd3116
nightnoryu <qazwf734@gmail.com>
2026-09-04 09:24:18 +0300

    Remove imports

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./styles/app.css";
 import {App} from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
