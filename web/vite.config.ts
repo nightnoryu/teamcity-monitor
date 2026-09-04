a13bf15cd7eb0046715817c2e86dd3b112d88404
nightnoryu <qazwf734@gmail.com>
2026-09-04 09:54:18 +0300

    Add dev dockerfile for web with vite config

---
+++ web/vite.config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    server: {
+        host: true,
+        port: 3000,
+        strictPort: true,
+        watch: {
+            usePolling: true,
+            interval: 100,
+        },
+    },
+});
