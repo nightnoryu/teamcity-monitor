9bec844bf7f7e7c965fe60bdc14095d8e32fa65e
nightnoryu <qazwf734@gmail.com>
2026-09-04 14:48:38 +0300

    Add react hooks plugin for eslint

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,13 +1,21 @@
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist", "node_modules", "*.config.js", "*.config.ts"] },
   ...tseslint.configs.recommended,
+  reactHooks.configs.flat.recommended,
   {
     files: ["**/*.{ts,tsx}"],
+    plugins: { "react-refresh": reactRefresh },
     languageOptions: { ecmaVersion: 2022, sourceType: "module" },
     rules: {
       "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
   },
 );
